1df9d3ebc7ad741b697ef26354992022e1e2d0c3
voznyukpavel <Pavel@34.34.17.153>
2021-08-15 12:54:50 +0300

    dummy

--- src/main/java/com/lux/generator/manager/DataCreator.java
+++ src/main/java/com/lux/generator/manager/DataCreator.java
@@ -5,16 +5,16 @@ import java.util.StringTokenizer;
 
 import com.lux.generator.model.DataModel;
 
-public class DataCreator implements IModelFactory<ArrayList<DataModel> >{
+public class DataCreator implements IModelFactory<ArrayList<DataModel>> {
 
-	@Override
-	public ArrayList<DataModel> create(String data) {
-		ArrayList<DataModel> projects = new ArrayList<>();
-		ModelFactory mf= new ModelFactory();
-		StringTokenizer st = new StringTokenizer(data, System.lineSeparator());
-		while (st.hasMoreTokens()) {
-			projects.add(mf.create(st.nextToken()));
-		}
-		return projects;
-	}
+    @Override
+    public ArrayList<DataModel> create(String data) {
+        ArrayList<DataModel> projects = new ArrayList<>();
+        ModelFactory mf = new ModelFactory();
+        StringTokenizer st = new StringTokenizer(data, System.lineSeparator());
+        while (st.hasMoreTokens()) {
+            projects.add(mf.create(st.nextToken()));
+        }
+        return projects;
+    }
 }
